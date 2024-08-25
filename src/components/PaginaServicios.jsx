e4d8718eb47a7c3524a5d580c2cddd86c2136d75
AntiKris <cristophermatiasmesina@gmail.com>
2024-08-24 22:27:35 -0400

    update

--- src/components/PaginaServicios.jsx
+++ src/components/PaginaServicios.jsx
@@ -4,21 +4,23 @@ import { Carousel } from './Carousel'
 export const PaginaServicios = () => {
   return (
     <>
-        <div className='flex flex-row ms:flex-wrap h-xxl'>
-            <div className='w-full text-center'>
-                <h1 className='text-white mx-auto mt-44 mb-5 text-7xl font-extrabold animate-fade-down animate-duration-[3000ms]'>Servicios</h1>
-                <p className='text-white animate-fade-down animate-duration-[3000ms]'>Aqui encontraras todos los proyectos y servicios que otorgamos</p>
+        <div  className='flex flex-row ms:flex-wrap h-xxl w-full'>
+            <div className='w-full text-center mx-10'>
+                <div className='w-60 h-full'>
+                    <ul className='my-auto list-none text-sm'>
+                        <h3 className='mx-auto text-2xl my-auto'>Listado de Servicios</h3>
+                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Planimetria y fabricacion</a></li>
+                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Transportadores de banda</a></li>
+                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Estructuras y plataformas de acero</a></li>
+                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Montaje de estructuras metalicas</a></li>
+                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Servicios de Corte CNC</a></li>
+                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Piping en Acero Inoxidable</a></li>
+                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Servicios de Soldadura</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
-        <div className='flex flex-row ms:flex-wrap'>
-            <div>
-                <h1>
-                    
-                </h1>
-            </div>
-            <Carousel/>
-            
-        </div>
+
     </>
     )
 }
