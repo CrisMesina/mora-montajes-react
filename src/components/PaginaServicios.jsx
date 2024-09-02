199e53366c51d396247883100eba6af7dc229322
AntiKris <cristophermatiasmesina@gmail.com>
2024-09-01 21:30:14 -0400

    Update Secccion Services

--- src/components/PaginaServicios.jsx
+++ src/components/PaginaServicios.jsx
@@ -3,24 +3,32 @@ import { Carousel } from './Carousel'
 
 export const PaginaServicios = () => {
   return (
-    <>
-        <div  className='flex flex-row ms:flex-wrap h-xxl w-full'>
             <div className='w-full text-center mx-10'>
-                <div className='w-60 h-full'>
-                    <ul className='my-auto list-none text-sm'>
-                        <h3 className='mx-auto text-2xl my-auto'>Listado de Servicios</h3>
-                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Planimetria y fabricacion</a></li>
-                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Transportadores de banda</a></li>
-                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Estructuras y plataformas de acero</a></li>
-                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Montaje de estructuras metalicas</a></li>
-                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Servicios de Corte CNC</a></li>
-                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Piping en Acero Inoxidable</a></li>
-                        <li className="my-8">➤<a className='hover:text-blue-500' href="/">Servicios de Soldadura</a></li>
-                    </ul>
-                </div>
+    <div className="flex flex-row ms:flex-wrap w-full pb-2 mt-20">
+        <div className='w-full h-screen'>
+            <div className='w-full' >
+                <h1 className='text-center font-extrabold text-6xl'>Servicios!</h1>
+                <p className='text-center my-5 text-2xl'>Aqui podras encontrar todos nuestros servicios disponibles!</p>
+            </div>
+            <div  className='w-full'>
+                <ul className='w-full'>
+                    <div  className='my-2 mx-23'>
+                        <a href="/" className='mx-2 underline hover:text-blue-700' >Planimetria y Fabricacion</a>
+                        <a href="/" className='mx-2 underline hover:text-blue-700' >Transportadores de banda</a>
+                        <a href="/" className='mx-2 underline hover:text-blue-700' >Estructuras y plataformas de acero</a>
+                        <a href="/" className='mx-2 underline hover:text-blue-700' >Montaje de Estructuras metalicas</a>
+                    </div>
+                    <div className='my-2 mx-23'>
+                        <a href="/" className='mx-2 my-5 underline hover:text-blue-700' >Servicios de Corte CNC</a>
+                        <a href="/" className='mx-2 my-5 underline hover:text-blue-700' >Piping de acero inoxidable</a>
+                        <a href="/" className='mx-2 my-5 underline hover:text-blue-700' >Servicios de soldadura</a>
+                    </div>
+                </ul>
             </div>
         </div>
-
-    </>
+        <div className='w-full h-screen'>   
+            s
+        </div>
+    </div>
     )
 }
